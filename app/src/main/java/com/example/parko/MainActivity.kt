f9c9776734a7719dba6f93ca6f122fb964846096
docfr10 <docfr10@yandex.ru>
2024-12-10 19:12:06 +0300

    Update TopAppBar

--- app/src/main/java/com/example/parko/MainActivity.kt
+++ app/src/main/java/com/example/parko/MainActivity.kt
@@ -5,11 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.parko.ui.screen.AppScreen
 import com.example.parko.ui.theme.ParkoTheme
 
@@ -20,9 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ParkoTheme {
                 Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(all = 16.dp),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppScreen()
--- app/src/main/java/com/example/parko/ui/screen/AppScreen.kt
+++ app/src/main/java/com/example/parko/ui/screen/AppScreen.kt
@@ -1,15 +1,25 @@
 package com.example.parko.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,7 +30,9 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun AppScreen(navController: NavHostController = rememberNavController()) {
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 16.dp),
         topBar = {
             TopAppBar(
                 modifier = Modifier.fillMaxWidth(),
@@ -29,9 +41,33 @@ fun AppScreen(navController: NavHostController = rememberNavController()) {
                     scrolledContainerColor = MaterialTheme.colorScheme.background,
                     actionIconContentColor = MaterialTheme.colorScheme.onBackground,
                     navigationIconContentColor = MaterialTheme.colorScheme.onBackground,
-                    titleContentColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
                 ),
-                title = { }
+                title = {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Image(
+                                imageVector = Icons.Default.Person,
+                                contentDescription = "User photo"
+                            )
+
+                            Column {
+                                Text(text = "Hello USERNAME!")
+                                Text(text = "Park Your Car")
+                            }
+
+                            Image(
+                                imageVector = Icons.Default.Notifications,
+                                contentDescription = "Notifications"
+                            )
+                        }
+                    }
+                }
             )
         }
     ) { paddingValues ->
