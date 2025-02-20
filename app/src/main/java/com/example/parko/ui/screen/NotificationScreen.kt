a4301909742722a694d78cc3ee35bfe8e91648c6
docfr10 <docfr10@yandex.ru>
2025-02-20 21:06:26 +0300

    Add preview annotation

--- app/src/main/java/com/example/parko/ui/screen/NotificationScreen.kt
+++ app/src/main/java/com/example/parko/ui/screen/NotificationScreen.kt
@@ -2,8 +2,10 @@ package com.example.parko.ui.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview(showBackground = true)
 fun NotificationScreen() {
     Text(text = "This is notification screen")
 }
